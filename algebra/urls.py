d01ed5a9d1ebffcee703b2d3dfc71646ceaa5b2c
Карандайкин Константин Алексеевич <K.Karandaykin@OCRV.RU>
2021-12-10 13:44:36 +0300

    updates 4 local test in win

--- algebra/urls.py
+++ algebra/urls.py
@@ -1,10 +1,10 @@
 from . import views
-from django.conf.urls import url
+from django.urls import include, re_path
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^new/(?P<et>(\w|-)+)/$', views.new, name='new'),
-    url(r'^expr/$', views.ExprListView.as_view(), name='expr'),
-    url(r'^eq/$', views.EquationListView.as_view(), name='eq'),
-    url(r'^expr/(?P<pk>(\w|-)+)/$', views.expr_solve_view, name='expr_solve'),
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^new/(?P<et>(\w|-)+)/$', views.new, name='new'),
+    re_path(r'^expr/$', views.ExprListView.as_view(), name='expr'),
+    re_path(r'^eq/$', views.EquationListView.as_view(), name='eq'),
+    re_path(r'^expr/(?P<pk>(\w|-)+)/$', views.expr_solve_view, name='expr_solve'),
 ]
--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -77,17 +77,22 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'djangoproject',
+        # 'USER': 'juc',
+        # 'PASSWORD': 'password',
+        # 'HOST': 'localhost',
+        # 'PORT': '',
+    # }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangoproject',
-        'USER': 'juc',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
 
